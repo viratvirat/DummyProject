85b04e981128b9b03a72b7ffb87e88b7d730ad03
upendra.bellamkonda <upendra.bellamkonda@HHCL-IT-12778.heterohcl.com>
2023-08-05 14:22:06 +0530

    Team Lead

---
+++ src/com/upendra/Sample.java
@@ -0,0 +1,22 @@
+package com.upendra;
+
+public class Sample {
+	public static void main(String[] args) {
+		System.out.println("dummy ");
+		System.out.println("dummy ");
+		System.out.println("dummy ");
+		System.out.println("dummy ");
+		System.out.println("dummy ");
+		System.out.println("dummy ");
+		System.out.println("dummy ");
+		System.out.println("dummy ");
+
+		System.out.println("dummy ");
+		System.out.println("dummy ");
+
+		System.out.println("dummy ");
+		System.out.println("dummy ");
+
+	}
+
+}
